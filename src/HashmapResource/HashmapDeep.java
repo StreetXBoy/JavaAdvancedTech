3d394006450efbbebe72f99f1e3e6244e0096b51
StreetXBoy <472102894@qq.com>
2020-12-02 21:20:31 -0800

    hashmap

---
+++ src/HashmapResource/HashmapDeep.java
@@ -0,0 +1,28 @@
+package HashmapResource;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author 重新做人idea基础学习
+ * @date 2020-12-02
+ */
+
+//https://www.jianshu.com/p/4aa3bb16f36c
+
+//    https://www.jianshu.com/p/2554b0cd169b  吊打阿里面试官
+
+//    https://blog.csdn.net/u012723673/article/details/80699029
+
+
+//     本文要介绍的是Java中的transient关键字，transient是短暂的意思。对于transient 修饰的成员变量，在类的实例对象的序列化处理过程中会被忽略。 因此，transient变量不会贯穿对象的序列化和反序列化，生命周期仅存于调用者的内存中而不会写到磁盘里进行持久化。
+
+
+
+
+public class HashmapDeep {
+    public static void main(String[] args) {
+        Map<Integer, Integer>map =new HashMap<>();
+
+    }
+}
